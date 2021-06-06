00a345c2c1261e72c681af4b0c50f6558719d24f
zeyadkhaled1 <1804794@eng.asu.edu.eg>
2021-06-06 17:27:44 +0200

    Update Distance.c

--- Distance.c
+++ Distance.c
@@ -1,18 +1,60 @@
+#include "stdint.h"
+#include <stdio.h>
+#include "math.h"
+#define pi 3.1415926535897932846
+float Total_Distance;
+double Latitude[2] = {0.0 , 0.0} ;
+double Longitude[2] = {0.0 , 0.0} ;
+int f = 0 ;
 
-	float total_distance(float * total_distance ,float lat1, float long1, float lat2, float long2)
-{
-	uint32_t R = 6371000;
-	double x1, x2, delta_x, delta_y;
-  double a, c, d;
-	x1 = lat1 * (PI / 180);
-  x2 = lat2 * (PI / 180);
-  delta_x = (lat2 - lat1) * (PI / 180);
-  delta_y = (long2 - long1) * (PI / 180);
+void SystemInit(){}
 
-  a = (sin(delta_x / 2) * sin(delta_x / 2)) + cos(x1) * cos(x2) * sin(delta_y / 2) * sin(delta_y / 2);
-  c = 2 * atan2(sqrt(a), sqrt(1-a));
-  d = R * c;
-	*total_distance = *total_distance + d; //total_distance must be initiallized by zero in the main !!!!!!
+double deg2rad(float deg) {
 
-	return d;
-}
+    return (deg * pi / 180);
+
+    }
+
+double rad2deg(float rad) {
+
+        return (rad * 180 / pi);
+
+    }
+
+
+double Distance(double Lat, double Long) {
+
+        if     (f == 1){[[]]
+
+             Latitude[1] = Lat ;
+            Longitude[1] = Long ;
+
+            if((Latitude[0] == Latitude[1]) && (Longitude[0] == Longitude[1]) ){
+                return Total_Distance;
+            }
+            double theta = Longitude[0] - Longitude[1];
+            double dist = sin(deg2rad(Latitude[0]))sin(deg2rad(Latitude[1]))+cos(deg2rad(Latitude[0]))cos(deg2rad(Latitude[1]))*cos(deg2rad(theta));
+            Total_Distance += rad2deg(acos(dist)) * 60 * 1.1515 * 1.609344 * 1000;
+
+            Latitude[0] = Lat ;
+            Longitude[0] = Long ;
+
+        }
+        else if (f==0){
+            f=1;
+            Latitude[0] = Lat ;
+            Longitude[0] = Long ;
+        }
+
+        return Total_Distance;
+    }
+   int main(){
+
+       Distance(0.00001    , 0.00001);
+       Distance(0.00002    ,0.00002);
+       printf("%f",Total_Distance);
+
+
+
+
+   }
